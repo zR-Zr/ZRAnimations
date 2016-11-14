6243fd10fd7660fb5137628adae21e8f54439a2e
zR-Zr <zR@hedademac-mini.local>
2016-11-14 08:56:59 +0800

    Initial Commit

---
+++ ZRAnimView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZRAnimView
+//
+//  Created by zR on 2016/11/14.
+//  Copyright © 2016年 zR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZRAnimView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZRAnimView
+//
+//  Created by zR on 2016/11/14.
+//  Copyright © 2016年 zR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
